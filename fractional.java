47692445f47b253c35abf92202c7db5e567063ad
srimuthaiah08 <athibairavisri@gmail.com>
2025-12-10 22:37:15 +0530

    Add files via upload

---
+++ fractional.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class word {
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Enter a fractional number: ");
+        double number = x.nextDouble(); 
+        System.out.println("You entered: " + number);
+}
+}
